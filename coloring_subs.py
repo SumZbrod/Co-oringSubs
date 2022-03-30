cd4449c53971f0de04965b496d5119a88ed6ae85
sumzbrod <sumzbrod@gmail.com>
2022-03-30 14:36:27 +0600

    mpv update

--- coloring_subs.py
+++ coloring_subs.py
@@ -6,32 +6,50 @@ import re
 import spacy
 nlp = spacy.load("ja_ginza")
 import platform
-
-
+import argparse
+import os
+import subprocess
+import sys 
 
 def load_str(path):
     with open(path) as f:
         return f.read()
 
+def get_slesh():
+    if platform.system() == 'Linux':
+        slesh_line = '/'
+    else:
+        slesh_line = '\\'
+    return slesh_line
+
 class PaitSub:
     def __init__(self, n_level=4):
+        slesh_line = get_slesh()
+        cwd = slesh_line.join(os.getcwd().split(slesh_line)[:3])+'/.config/mpv/scripts/subscoloring/'.replace('/', slesh_line)
         self.n_level = n_level
         self.g_stat  = [] 
         self.rg_stat = [] 
         self.v_stat  = [] 
         for n in range(5, n_level-1, -1):
-            self.g_stat.append(load_str(f'ndata{slesh_line}n{n}G').splitlines())    
-            self.rg_stat.append(load_str(f'ndata{slesh_line}n{n}Gr').splitlines())   
-            self.v_stat.append(load_str(f'ndata{slesh_line}n{n}V').splitlines())    
+            self.g_stat.append(load_str( f'{cwd}ndata{slesh_line}n{n}G').splitlines())    
+            self.rg_stat.append(load_str(f'{cwd}ndata{slesh_line}n{n}Gr').splitlines())   
+            self.v_stat.append(load_str( f'{cwd}ndata{slesh_line}n{n}V').splitlines())    
         self.g_palette = ['#fafa6e', '#f7ca3d', '#f09819', '#e3630e', '#d01919'][:5-n_level+1]
         self.v_palette = ['#82fa6e', '#00deb9', '#00b7ff', '#0082ff', '#6f19d0'][:5-n_level+1]
 
-    def coloring(self, corpus):
+    def coloring(self, corpus, sub_ex):
         new_corpus = ''
         g_count = [0] * (6-self.n_level)
         v_count = [0] * (6-self.n_level)
         for line in corpus.splitlines():
-            if len(line)>1 and not line.isnumeric() and '-->' not in line:
+            if sub_ex == 'srt':
+                flag_preparing = len(line)>1 and not line.isnumeric() and '-->' not in line
+            else:
+                flag_preparing = line[:8] == 'Dialogue'
+            if flag_preparing:
+                if sub_ex == 'ass':
+                    line = line.split(',,')[-1]
+                    pre_line = ',,'.join(line.split(',,')[:-1])
                 # print(line)
                 line = line.replace('…', 'ð')
                 doc = nlp(line)
@@ -59,35 +77,35 @@ class PaitSub:
                         for part in re.findall(r_g, new_line):
                             new_line = new_line.replace(part, f'<font color="{self.g_palette[n]}">{part}</font>')
                             g_count[n] += 1
-                            print(part)
                 new_line = new_line.replace('ð', '…')
                 
 
             else:
                 new_line = line
+            if flag_preparing and sub_ex == 'ass':
+                new_line = pre_line + new_line
             new_corpus += new_line + '\n'
         print('grammar: ', g_count)
         print('vocabulary: ', v_count)
         return new_corpus
 
 def main():
-    if platform.system() == 'Linux':
-        slesh_line = '/'
-    else:
-        slesh_line = '\\'
-
-    # str_path = '/home/nikita/Downloads/Sono_Bisque_Doll_wa_Koi_wo_Suru/My.Dress-Up.Darling.S01E06.WEBRip.Netflix.ja[cc].srt'
-    str_path = input('path to the srt subtitles: ')
-    # str_path = '/home/nikita/Downloads/n/1-26/10.srt'
-    str_path = str_path + slesh_line
-    str_data = load_str(str_path)
+    
+    slesh_line = get_slesh()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("subs_path")
+    subs_path = parser.parse_args().subs_path
+    sub_ex = subs_path[-3:]
+    subs_path = subs_path
+    if 'file://' in subs_path:
+        subs_path = subs_path[7:]
+    str_data = load_str(subs_path)
 
     P = PaitSub(3)
 
-    new_corpus = P.coloring(str_data)
+    new_corpus = P.coloring(str_data, sub_ex)
 
-    new_path =  str_path[:-4] + 'c.srt'
-    # print(new_path)
+    new_path =  subs_path[:-4] + '_c.' + sub_ex
     with open(new_path, 'w') as f:
         f.write(new_corpus)
 
---
+++ highlight_sub.lua
@@ -0,0 +1,149 @@
+-- Usage:
+--  default keybinding: n
+--  add the following to your input.conf to change the default keybinding:
+--  keyname script_binding highlight_sub-menu
+
+local mp = require('mp')
+local utils = require('mp.utils')
+local sub = require('subtitle')
+
+-- Config
+-- Options can be changed here or in a separate config file.
+-- Config path: ~/.config/mpv/script-opts/highlight_sub.conf
+local config = {
+    -- Change the following lines if the locations of executables differ from the defaults
+    -- If set to empty, the path will be guessed.
+    ffmpeg_path = "",
+    ffsubsync_path = "",
+    alass_path = "",
+
+    -- Choose what tool to use. Allowed options: ffsubsync, alass, ask.
+    -- If set to ask, the add-on will ask to choose the tool every time.
+    audio_subsync_tool = "ask",
+    altsub_subsync_tool = "ask",
+
+    -- After retiming, tell mpv to forget the original subtitle track.
+    unload_old_sub = true,
+}
+
+-- Snippet borrowed from stackoverflow to get the operating system
+-- originally found at: https://stackoverflow.com/a/30960054
+local os_name = (function()
+    if os.getenv("HOME") == nil then
+        return function()
+            return "Windows"
+        end
+    else
+        return function()
+            return "*nix"
+        end
+    end
+end)()
+
+local os_temp = (function()
+    if os_name() == "Windows" then
+        return function()
+            return os.getenv('TEMP')
+        end
+    else
+        return function()
+            return '/tmp/'
+        end
+    end
+end)()
+
+local function notify(message, level, duration)
+    level = level or 'info'
+    duration = duration or 1
+    mp.msg[level](message)
+    mp.osd_message(message, duration)
+end
+
+local function subprocess(args)
+    return mp.command_native {
+        name = "subprocess",
+        playback_only = false,
+        capture_stdout = true,
+        args = args
+    }
+end
+
+local function get_active_track(track_type)
+    local track_list = mp.get_property_native('track-list')
+    for num, track in ipairs(track_list) do
+        if track.type == track_type and track.selected == true then
+            return num, track
+        end
+    end
+    return notify(string.format("Error: no track of type '%s' selected", track_type), "error", 3)
+end
+
+local function remove_extension(filename)
+    return filename:gsub('%.%w+$', '')
+end
+
+local function get_extension(filename)
+    return filename:match("^.+(%.%w+)$")
+end
+
+local function mkfp_c(sub_path)
+    return table.concat { remove_extension(sub_path), '_c', get_extension(sub_path) }
+end
+
+local function extract_to_file(subtitle_track)
+    local codec_ext_map = { subrip = "srt", ass = "ass" }
+    local ext = codec_ext_map[subtitle_track['codec']]
+    if ext == nil then
+        return notify(string.format("Error: unsupported codec: %s", subtitle_track['codec']), "error", 3)
+    end
+    local temp_sub_fp = utils.join_path(os_temp(), 'highlight_sub_extracted.' .. ext)
+    notify("Extracting internal subtitles...", nil, 3)
+    local ret = subprocess {
+        config.ffmpeg_path,
+        "-hide_banner",
+        "-nostdin",
+        "-y",
+        "-loglevel", "quiet",
+        "-an",
+        "-vn",
+        "-i", mp.get_property("path"),
+        "-map", "0:" .. (subtitle_track and subtitle_track['ff-index'] or 's'),
+        "-f", ext,
+        temp_sub_fp
+    }
+    if ret == nil or ret.status ~= 0 then
+        return notify("Couldn't extract internal subtitle.\nMake sure the video has internal subtitles.", "error", 7)
+    end
+    return temp_sub_fp
+end
+
+local function highlight_subs()
+    local _, track = get_active_track('sub')
+    local file_path = track.external and track['external-filename'] or extract_to_file(track)
+    -- os.execute"python "...file_path
+    local cwd = os.getenv("PWD")..'/'
+    local codec_parser_map = { ass = sub.ASS, subrip = sub.SRT }
+    local parser = codec_parser_map[track['codec']]
+    -- local exe_command = "python ~/.config/mpv/scripts/subscoloring/coloring_subs.py "..cwd..file_path 
+    local exe_command = "python ~/.config/mpv/scripts/subscoloring/coloring_subs.py "..'"'..file_path..'"' 
+    -- print(1)
+    -- print(cwd)
+    -- print(file_path)
+
+    -- print("python ~/.config/mpv/scripts/subscoloring/coloring_subs.py "..file_path)
+    -- print(1)
+    file_path = file_path:gsub('file://', '')
+    -- print(mkfp_c(file_path))
+    -- print(exe_command)
+    notify("Coloring...")
+    os.execute(exe_command)
+    -- print(file_path)
+
+    local s = parser:populate(mkfp_c(file_path))
+
+    mp.commandv("sub_add", s.filename)
+    s:save()
+    notify("Complited")
+end
+
+mp.add_forced_key_binding("Alt+c", "highlight_subs", highlight_subs)
